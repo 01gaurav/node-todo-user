d3906d08a2c3fbd9a4db53c8cea5754930125f8a
master <011999gv@gmail.com>
2020-06-20 15:44:42 +0530

    user model

--- server/server.js
+++ server/server.js
@@ -96,6 +96,18 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    res.send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
