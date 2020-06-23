aeb26564d2f820e1a4a42e8ecb652f3c0bdc4869
master <011999gv@gmail.com>
2020-06-23 11:49:59 +0530

    added test for user

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,7 +1,7 @@
 const expect = require('expect');
 const request = require('supertest');
 const {ObjectID} = require('mongodb');
-
+const {User} = require('./../models/user');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const {todos,populatetodos,users,populateUsers} = require('./seed/seed');
@@ -166,6 +166,79 @@ describe('PATCH /todos/:id', () => {
         expect(res.body.todo.completed).toBe(false);
         expect(res.body.todo.completedAt).toNotExist();
       })
+      .end(done);
+  });
+});
+
+describe('GET /users/me',()=>{
+  it('should return user if authenticated',(done)=>{
+    request(app)
+      .get('/users/me')
+      .set('x-auth',users[0].tokens[0].token)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return 401 if not authenticated',(done)=>{
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res)=>{
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+});
+
+describe('POST /users',()=>{
+  it('should create a user', (done)=>{
+    var email = 'example@gmail.com';
+    var password = '123mnb';
+
+    request(app)
+      .post('/users')
+      .send({email,password})
+      .expect(200)
+      .expect((res)=>{
+        expect(res.headers['x-auth']).toExist();
+        expect(res.headers[res.body._id]).toExist();
+        expect(res.body.email).toBe(email);
+      })
+      .end((err)=>{
+        if(err){
+          return done(err);
+        }
+        User.findOne({email}).then((user)=>{
+          expect(user).toExist();
+          expect(user.password).toNotBe(password);
+          done();
+        });
+      });
+  });
+
+  it('should return validation errors if request invalid',(done)=>{
+    request(app)
+      .post('/users')
+      .send({
+        email:'and',
+        password:'09'
+      })
+      .expect(400)
+      .end(done);
+  });
+
+  it('should not create user if email in use',(done)=>{
+    request(app)
+      .post('/users')
+      .send({
+        email:users[0].email,
+        password: 'Password123'
+      })
+      .expect(400)
       .end(done);
   });
 });
